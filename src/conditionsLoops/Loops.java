fb8ca8b22970601eccd6ae33bd8132f77c6a8dfb
RICHA JHA <jricha283@gmail.com>
2023-10-25 13:20:37 +0530

    Loops

---
+++ src/conditionsLoops/Loops.java
@@ -0,0 +1,13 @@
+package conditionsLoops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		//Print numbers from 1 to 5
+		for(int num = 1; num <=5; num++)
+		{
+			System.out.println(num);
+		}
+	}
+
+}
