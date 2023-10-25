2feb6696e8a53c5a5915eede9b51d3cb2e90e55f
RICHA JHA <54667064+richa283@users.noreply.github.com>
2023-10-25 13:24:20 +0530

    Update Loops.java

--- src/conditionsLoops/Loops.java
+++ src/conditionsLoops/Loops.java
@@ -3,11 +3,16 @@ package conditionsLoops;
 public class Loops {
 
 	public static void main(String[] args) {
+		/*Syntax of for loops:
+  		for(initialization; condition; increment/decrement){
+    		//body
+		}
+  		*/
+		
 		//Print numbers from 1 to 5
 		for(int num = 1; num <=5; num++)
 		{
 			System.out.println(num);
 		}
 	}
-
 }
